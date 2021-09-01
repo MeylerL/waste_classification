0e9acbc670bcac27d4945a1274def0e9750eb40d
Izzy Weber <izzyweber@gmail.com>
2021-09-01 11:37:08 +0100

    gcp get_data funcs for taco and trashnet

--- waste_classification/data.py
+++ waste_classification/data.py
@@ -1,3 +1,5 @@
+from google.cloud import storage
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,13 +7,13 @@ import os
 import PIL
 import PIL.Image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+from waste_classification.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH_TACO, BUCKET_TRAIN_DATA_PATH_TRASHNET
 
-
-def load_trashnet(colab=False):
-  "loads trashnet data from files within raw_data folder.Returns a train_ds and val_ds as pandas dataframes."
-  if colab:
-    #uses original dataset since on virtual machine
-    directory = "/content/dataset-original"
+def load_trashnet(gcp=False):
+  """loads trashnet data from files within gcp if gcp True. Otherwise loads from local dataset.
+  Returns a train_ds and val_ds as pandas dataframes."""
+  if gcp:
+    directory = "gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH_TRASHNET}"
   else:
     #uses resized dataset on local computer
     directory = "../raw_data/dataset-resized/"
@@ -35,12 +37,11 @@ def load_trashnet(colab=False):
   return train_ds, val_ds
 
 
-def load_tacos(colab=False):
-  "loads TACO data from files within raw_data/TACOS/data folder.Returns a train_ds and val_ds as pandas dataframes."
-  if colab:
-    #uses original dataset since on virtual machine
-    #paper, plastic, trash, cardboard, metal are in a folder called "cat_fodlers" under /content.
-    directory = "/content/cat_folders"
+def load_TACO(gcp=False):
+  """loads TACOS data from files within gcp if gcp True. Otherwise loads from local dataset.
+  Returns a train_ds and val_ds as pandas dataframes."""
+  if gcp:
+    directory = "gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH_TACO}"
   else:
     #uses dataset on local computer. For this, make sure that the folders called
     #paper, plastic, trash, cardboard, metal are in a folder called "cat_fodlers" under TACO/data
@@ -64,3 +65,14 @@ def load_tacos(colab=False):
       image_size=(img_height, img_width),
       batch_size=batch_size)
   return train_ds, val_ds
+
+
+def get_data_TACO():
+    '''returns a TACO train_ds, val_ds'''
+    train_ds, val_ds = load_TACO(gcp=True)
+    return train_ds, val_ds
+
+def get_data_trashnet():
+    '''returns a trashnet train_ds, val_ds'''
+    train_ds, val_ds = load_trashnet(gcp=True)
+    return train_ds, val_ds
--- waste_classification/utils.py
+++ waste_classification/utils.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import json
 import shutil
+#----------------------------
+#METRICS GO HERE
+#----------------------------
 
+
+
+
+
+#----------------------------
+#TACO JSON PARSER
+#----------------------------
 # dict converting taco numbered categories into trashnet string categories
 category_conversion = {}
 category_conversion['metal'] = [0, 8, 10, 11, 12, 28]
@@ -12,7 +22,6 @@ category_conversion['plastic'] = [4, 5, 7, 24, 27, 43, 44, 47, 49, 55]
 category_conversion['trash'] = [1, 2, 3, 22, 25, 29, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46,
                                 48, 50, 51, 52, 53, 54, 57, 58, 59]
 
-
 def move_rename_jsons(my_path):
     """moves the jsons into one directory and renames them by batch"""
     # you will need to choose or create a directory called "annotations" inside TACO/data for second part of shutil
@@ -20,7 +29,6 @@ def move_rename_jsons(my_path):
         shutil.copy(f'{my_path}/TACO/data/batch_{i}/annotations.json',
                     f'{my_path}/TACO/data/annotations/batch_{i}_annotations.json')
     return None
-
 
 def open_json(n, my_path):
     """opens the json for taco batch and extracts the image and category data"""
@@ -30,14 +38,12 @@ def open_json(n, my_path):
     category_data = data['annotations']
     return image_data, category_data
 
-
 def image_ids(image_data):
     """ returns a dict with image_ids and image file names"""
     image_files = {}
     for item in image_data:
         image_files[item['id']] = item['file_name']
     return image_files
-
 
 def image_categories(category_data):
     """ returns a dict with the image_ids and the taco category numbers
@@ -51,7 +57,6 @@ def image_categories(category_data):
     for key in image_categories_dict:
         image_categories_dict[key] = list(set(image_categories_dict[key]))
     return image_categories_dict
-
 
 def compatible_images(image_categories):
     """ returns a dict with image_id and category, only includes images that
@@ -79,7 +84,6 @@ def compatible_images(image_categories):
                     if len(v) <= 1}
     return final_images
 
-
 def move_images(n, final_images, image_files, my_path):
     """creates dataframe of images to be moved and moves them to relevant
     trashnet folder"""
@@ -95,7 +99,6 @@ def move_images(n, final_images, image_files, my_path):
         shutil.copy(f'{my_path}/TACO/data/batch_{n}/{row["filename"]}',
                     f'{my_path}/TACO/data/{row["category"]}/batch_{n}{row["filename"]}')
     return None
-
 
 def get_tacos(my_path):
     """loops through all jsons and applies the above functions in order to unpack
