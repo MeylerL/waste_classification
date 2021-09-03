ab1c6cf60459fe1542d863ec6f0a1dc55f6f1938
Izzy Weber <izzyweber@gmail.com>
2021-09-03 10:36:00 +0100

    os path replaces absolute references to Lucys directores

--- waste_classification/params.py
+++ waste_classification/params.py
@@ -1,15 +1,22 @@
+import os
+package_parent = os.path.dirname(os.getcwd())
+package_directory = os.getcwd()
+#general paths:
+TRASHNET_RESIZED = os.path.join(package_parent, "raw_data", "dataset-resized")
 #for GCP
 BUCKET_NAME = "wagon-data-699-waste_classification"
-LOCAL_PATH_TRASHNET = "/Users/Lucy/code/MeylerL/waste_classification/raw_data/dataset-original"
-LOCAL_PATH_TACO = "/Users/Meylerl/code/MeylerL/waste_classification/raw_data/TACO/data/cat_folders"
+LOCAL_PATH_TRASHNET = os.path.join(package_parent, "raw_data", "dataset-original")
+LOCAL_PATH_TACO = os.path.join(package_parent, "raw_data", "TACO", "trainDataTACO")
 BUCKET_TRAIN_DATA_PATH_TRASHNET = f"waste_management_data/{LOCAL_PATH_TRASHNET}"
 BUCKET_TRAIN_DATA_PATH_TACO = f"waste_management_data/{LOCAL_PATH_TACO}"
-TACO_FOLDER_NAME = f""
-TRASHNET_FOLDER_NAME = f""
 BUCKET_FOLDER = "waste_management_data"
 TRASHNET_BUCKET_FILE_NAME = "dataset-original"
 TACO_BUCKET_FILE_NAME = "cat_folder"
 
 #for taco cropping:
-TACO_path = "../raw_data/TACO/"
-annotations_path = "../raw_data/TACO/data/annotations.json"
+TACO_path = os.path.join(package_parent, "raw_data", "TACO")
+annotations_path = os.path.join(package_parent, "raw_data", "TACO", "data", "annotations.json")
+
+if __name__ == "__main__":
+  test= os.path.join(package_parent, "raw_data", "dataset-resized")
+  print(test, os.path.exists(test))
