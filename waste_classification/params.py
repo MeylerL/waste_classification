9b2ca9e57d25cc4f7eb7e6511588f3ef38c564d8
Xin Zhan <jessiexinz@gmail.com>
2021-09-03 17:15:37 +0100

    trashnet on gcp works

--- waste_classification/params.py
+++ waste_classification/params.py
@@ -11,10 +11,10 @@ TRASHNET_RESIZED = os.path.join(package_parent, "raw_data", "dataset-resized")
 BUCKET_NAME = "wagon-data-699-waste_classification"
 LOCAL_PATH_TRASHNET = os.path.join(package_parent, "raw_data", "dataset-original")
 LOCAL_PATH_TACO = os.path.join(package_parent, "raw_data", "TACO", "trainDataTACO")
-BUCKET_TRAIN_DATA_PATH_TRASHNET = f"waste_management_data/{LOCAL_PATH_TRASHNET}"
+# BUCKET_TRAIN_DATA_PATH_TRASHNET = f"waste_management_data/{LOCAL_PATH_TRASHNET}"
 BUCKET_TRAIN_DATA_PATH_TACO = f"waste_management_data/{LOCAL_PATH_TACO}"
 BUCKET_FOLDER = "waste_management_data"
-TRASHNET_BUCKET_FILE_NAME = "dataset-original"
+TRASHNET_BUCKET_PREFIX = "tensorflow_datasets/trashnet"
 TACO_BUCKET_FILE_NAME = "cat_folder"
 
 #for taco cropping:
