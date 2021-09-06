c7353ab2a2bf0fc0bff607919176f044f45f9526
Izzy Weber <izzyweber@gmail.com>
2021-09-06 16:34:30 +0100

    added code to make input image into a 180 by 180 by 3 np array

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import streamlit as st
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 
 st.set_page_config(
     page_title="Waste Classifier",
@@ -10,7 +10,6 @@ st.set_page_config(
     initial_sidebar_state="auto")  # collapsed
 
 st.markdown("""# ♻️ WASTE CLASSIFIER ♻️
-
 
 #### “There is no such thing as ‘away’. When we throw anything away it must go somewhere.”
 – Annie Leonard""")
@@ -22,23 +21,20 @@ st.text("")
 columns = st.columns(6)
 cardboard_image = Image.open('images/cardboard44.jpg')
 cardboard = columns[0].image(cardboard_image, caption='Cardboard', use_column_width=True)
-
 glass_image = Image.open('images/glass9.jpg')
 glass= columns[1].image(glass_image, caption='Glass', use_column_width=True)
-
 metal_image = Image.open('images/metal28.jpg')
 metal = columns[2].image(metal_image, caption='Metal', use_column_width=True)
-
 paper_image = Image.open('images/paper24.jpg')
 paper = columns[3].image(paper_image, caption='Paper', use_column_width=True)
-
 plastic_image = Image.open('images/plastic14.jpg')
 plastic = columns[4].image(plastic_image, caption='Plastic', use_column_width=True)
-
 trash_image = Image.open('images/trash113.jpg')
 trash = columns[5].image(trash_image, caption='Trash', use_column_width=True)
+
 st.text("")
 st.text("")
+
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 uploaded_file = st.file_uploader("Choose a image of waste for classification:", type="JPG")
@@ -47,31 +43,41 @@ if uploaded_file is not None:
     import time
     'Activating neural networks...'
 
-    latest_iteration = st.empty()
-    bar = st.progress(0)
+    # latest_iteration = st.empty()
+    # bar = st.progress(0)
+    # for i in range(100):
+    #     latest_iteration.text(f'Iteration {i+1}')
+    #     bar.progress(i + 1)
+    #     time.sleep(0.1)
 
-    for i in range(20):
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
+    X_test = Image.open(uploaded_file)
+    new_size = (180, 180)
+    X_test= X_test.resize(new_size)
+    st.write("Resizing image... new size is:")
+    st.write(X_test.size)
+    npframe = np.array(X_test.getdata())
+    imgrgb_df = pd.DataFrame(npframe)
+    st.image(X_test)
+    imgrgb_df = imgrgb_df.to_numpy().reshape((180, 180, 3))
+    st.write("Reshaping image... new shape is:")
+    st.write(imgrgb_df.shape)
 
-    st.success('Image classified!')
-st.text("")
+    #st.success('Image classified!')
+
 st.text("")
 st.text("")
 
 st.markdown("# Our Data")
 """Many thanks to Pedro Proença and Pedro Simões of tacodataset.org as well as Gary Thung of trashnet for thier invaluable open source data."""
 
-#TEAM
 st.markdown("# Our Team")
 columns = st.columns(4)
 xin_image = Image.open("images/xin.jpeg")
-xin = columns[2].image(xin_image, caption='Xin Zhan', use_column_width=True)
+xin = columns[0].image(xin_image, caption='Xin Zhan', use_column_width=True)
 lucy_image = Image.open("images/lucy.jpeg")
-lucy = columns[0].image(lucy_image, caption='Lucy Meyler', use_column_width=True)
+lucy = columns[1].image(lucy_image, caption='Lucy Meyler', use_column_width=True)
 jack_image = Image.open("images/jack.jpeg")
-jack = columns[1].image(jack_image, caption='Jack Riddleston', use_column_width=True)
+jack = columns[2].image(jack_image, caption='Jack Riddleston', use_column_width=True)
 izzy_image = Image.open("images/izzy.JPG")
 izzy = columns[3].image(izzy_image, caption='Izzy Weber', use_column_width=True)
 
@@ -82,3 +88,12 @@ st.sidebar.write(f"""
     [Our Data](/#our-data)\n
     """)
 
+# v  # creat sidebar
+# hard_coded_toc = '''<h1>Waste Classification</h1>
+#             <a href="#Intro">Intro</a><br>
+#             <a href="#Magic commands">Magic commands</a><br>
+#             <a href="#Inline documentation">Inline documentation</a><br>
+#             <a href="#About">About</a><br>
+#         '''
+
+# st.sidebar.markdown(hard_coded_toc, unsafe_allow_html=True)
